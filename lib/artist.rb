a0c42a268615a2b6671018343a12ba5aeb2e5651
Preston Davis <prestonseandavis101@yahoo.com>
2020-09-17 03:13:38 +0000

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,37 @@
+require 'pry'
+
+class Artist
+  attr_accessor :name, :songs
+  
+  
+  def initialize(name)
+    @name= name
+#binding.pry
+
+  end
+  
+  def songs
+    Song.all.collect do |song|
+    
+      song.artist == self
+      
+      song
+
+    
+    end
+  end
+  
+  def add_song(song)
+    song.artist = self
+  end
+   
+   def add_song_by_name(song_name)
+     singer = Song.new(song_name)
+     add_song(singer)
+   end
+   
+   def self.song_count
+    Song.all.count
+   
+   end
+end
