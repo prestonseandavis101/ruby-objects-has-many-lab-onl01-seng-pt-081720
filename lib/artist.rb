9bb505df5fbb640a540fa73f0affdd5351776d88
Preston Davis <prestonseandavis101@yahoo.com>
2020-09-17 02:06:00 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -24,5 +24,9 @@ class Artist
   def add_song(song)
     song.artist = self
   end
-  
+   
+   def add_song_by_name(song_name)
+     singer = Song.new(song_name)
+     add_song(singer)
+   end
 end
